067706d8023254f722112e7bd47163a77fff9954
Gowtham Kumar <uiuxdijain@gmail.com>
2021-05-29 17:37:05 +0530

    changed public api file place.
    changed public api file place.

---
+++ projects/my-lib/public-api.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of my-lib
+ */
+
+export * from './src/lib/my-lib.service';
+export * from './src/lib/my-lib.component';
+export * from './src/lib/my-lib.module';
+export * from './src/lib/custom/custom.component';
